49fb994b9fa7c81044c8afa704e0f9b5436f9ed5
Jonatas Evangelista <jonatasge@gmail.com>
2023-01-09 16:19:58 -0300

    feat: adds line chart on chart page

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { ChartService } from 'src/services/data/chart.service';
 import { DateService } from 'src/services/data/date.service';
 import { ChartGetResponse } from 'src/services/domain/chart';
@@ -12,8 +13,12 @@ import { ChartGetResponse } from 'src/services/domain/chart';
 export class ChartComponent {
   symbol = '';
   dataSource = [];
-  displayedColumns = ['index', 'date', 'open', 'close'];
+  displayedColumns = ['index', 'date', 'open', 'close', 'variation'];
   isLoading = false;
+
+  // chart
+  lineChartData: ChartConfiguration<'line'>['data'] = { datasets: [] };
+  lineChartOptions: ChartOptions<'line'> = {};
 
   constructor(
     private chartService: ChartService,
@@ -26,8 +31,11 @@ export class ChartComponent {
 
   async ngOnInit() {
     this.isLoading = true;
+
     const data = await this.loadData();
     this.dataSource = await this.handleData(data);
+    this.handleLineChartData(this.dataSource);
+
     this.isLoading = false;
   }
 
@@ -42,13 +50,35 @@ export class ChartComponent {
   async handleData(data: ChartGetResponse): Promise<any> {
     return new Promise((resolve) => {
       resolve(
-        data.reverse().map(({ date, open, close }, i) => ({
-          index: i + 1,
-          date: new Date(date).toLocaleDateString('pt-BR'),
-          open: parseFloat(open).toFixed(2),
-          close: parseFloat(close).toFixed(2),
-        }))
+        data.reverse().map(({ date, open, close }, i) => {
+          const o = parseFloat(open);
+          const c = parseFloat(close);
+
+          return {
+            index: i + 1,
+            date: new Date(date).toLocaleDateString('pt-BR'),
+            open: o.toFixed(2),
+            close: c.toFixed(2),
+            variation: (o - c).toFixed(2),
+          };
+        })
       );
     });
+  }
+
+  handleLineChartData(data: any[]) {
+    this.lineChartData = {
+      labels: data.map(({ date }) => date),
+      datasets: [
+        {
+          data: data.map(({ variation }) => variation),
+          label: 'Variação',
+        },
+      ],
+    };
+  }
+
+  isVariationUp(variation: string): boolean {
+    return parseFloat(variation) > 0;
   }
 }
